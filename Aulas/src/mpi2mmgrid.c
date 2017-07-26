da1c4a9481d1039da834ccfcf04b199ed41edff0
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2017-07-26 17:18:55 -0300

    grid created

--- Aulas/src/mpi2mmgrid.c
+++ Aulas/src/mpi2mmgrid.c
@@ -1,17 +1,3 @@
-/*
-00 01 02 03 04 05 06 07 08 09 10 11 12
-01 00 01 00 01 02 03 04 05 06 08 02 00
-03 02 05 06 03 04 07 06 07 
-12 04 11 04 05 08 09 08 10 
-            07
-
-
-00 01 02 03 04 05 06 07 08
-01 02 05 06 05 08 07 08 10
-03 04 11 04 07    09      
-12
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -25,28 +11,28 @@
 MPI_Group world_group;
 
 // Construct a group containing all of the ranks in world_group
-MPI_Group groups[13];
+MPI_Group groups[16];
 
 // Create a new communicator based on the group
-MPI_Comm rank_comm[13], directions[4];
+MPI_Comm rank_comm[16], directions[4];
 
 // Ranks relation
 const int ranks[][2] = {{ 0,  1}, // 00
 						{ 0,  3}, // 01
-						{ 0, 12}, // 02
+						{ 0,  9}, // 02
 						{ 1,  2}, // 03
 						{ 1,  4}, // 04
 						{ 2,  5}, // 05
-						{ 2, 11}, // 06
+						{ 2, 10}, // 06
 						{ 3,  4}, // 07
 						{ 3,  6}, // 08
 						{ 4,  5}, // 09
 						{ 4,  7}, // 10
 						{ 5,  8}, // 11
 						{ 6,  7}, // 12
-						{ 6,  9}, // 13
+						{ 6, 12}, // 13
 						{ 7,  8}, // 14
-						{ 8, 10}  // 15
+						{ 8, 11}  // 15
 					};
 
 
@@ -55,6 +41,28 @@ int world_rank, world_size;
 
 void init_ranks() {
 	switch(world_rank) {
+		/*
+		R    U   D   L   R
+		00 { 01, 02, -1, 00 }
+		01 { 04, -1, 00, 03 }
+		02 { 05, 06, 03, -1 }
+		03 { 08, 01, -1, 07 }
+		04 { 10, 04, 07, 09 }
+		05 { 11, 05, 09, -1 }
+		06 { 13, 08, -1, 12 }
+		07 { -1, 10, 12, 14 }
+		08 { 15, 11, 14, -1 }
+		09 { 02, -1, -1, -1 }
+		10 { 06, -1, -1, -1 }
+		11 { -1, 15, -1, -1 }
+		12 { -1, 13, -1, -1 }
+
+		   0  1  2  3  4  5  6  7  8  9 10 11 12
+		U 03 04 05 06 07 08 12 -1 11 00 02 -1 -1
+		D 09 -1 10 00 01 02 03 04 05 -1 -1 08 06
+		L -1 00 01 -1 03 04 -1 06 07 -1 -1 -1 -1
+		R 01 02 -1 04 05 -1 07 08 -1 -1 -1 -1 -1
+		*/
 
 		case 0: {
 			directions[UP] = rank_comm[1];
@@ -85,64 +93,64 @@ void init_ranks() {
 		} break;
 
 		case 4: {
-			directions[UP] = rank_comm[];
-			directions[DOWN] = rank_comm[];
-			directions[LEFT] = rank_comm[];
-			directions[RIGHT] = rank_comm[];
+			directions[UP] = rank_comm[10];
+			directions[DOWN] = rank_comm[4];
+			directions[LEFT] = rank_comm[7];
+			directions[RIGHT] = rank_comm[9];
 		} break;
 
 		case 5: {
-			directions[UP] = rank_comm[];
-			directions[DOWN] = rank_comm[];
-			directions[LEFT] = rank_comm[];
+			directions[UP] = rank_comm[11];
+			directions[DOWN] = rank_comm[5];
+			directions[LEFT] = rank_comm[9];
 			directions[RIGHT] = MPI_COMM_NULL;
 		} break;
 
 		case 6: {
-			directions[UP] = rank_comm[];
-			directions[DOWN] = rank_comm[];
+			directions[UP] = rank_comm[13];
+			directions[DOWN] = rank_comm[8];
 			directions[LEFT] = MPI_COMM_NULL;
-			directions[RIGHT] = rank_comm[];
+			directions[RIGHT] = rank_comm[12];
 		} break;
 
 		case 7: {
 			directions[UP] = MPI_COMM_NULL;
-			directions[DOWN] = rank_comm[];
-			directions[LEFT] = rank_comm[];
-			directions[RIGHT] = rank_comm[];
+			directions[DOWN] = rank_comm[10];
+			directions[LEFT] = rank_comm[12];
+			directions[RIGHT] = rank_comm[14];
 		} break;
 
 		case 8: {
-			directions[UP] = rank_comm[];
-			directions[DOWN] = rank_comm[];
-			directions[LEFT] = rank_comm[];
+			directions[UP] = rank_comm[15];
+			directions[DOWN] = rank_comm[11];
+			directions[LEFT] = rank_comm[14];
 			directions[RIGHT] = MPI_COMM_NULL;
 		} break;
 
 		case 9: {
-			directions[UP] = MPI_COMM_NULL;
-			directions[DOWN] = rank_comm[];
+			directions[UP] = rank_comm[2];
+			directions[DOWN] = MPI_COMM_NULL;
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
 		} break;
 
 		case 10: {
-			directions[UP] = MPI_COMM_NULL;
-			directions[DOWN] = rank_comm[];
+			directions[UP] = rank_comm[6];
+			directions[DOWN] = MPI_COMM_NULL;
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
 		} break;
 
 		case 11: {
-			directions[UP] = rank_comm[];
-			directions[DOWN] = MPI_COMM_NULL;
+			directions[UP] = MPI_COMM_NULL;
+			directions[DOWN] = rank_comm[15];
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
 		} break;
 
 		case 12: {
-			directions[UP] = rank_comm[];
-			directions[DOWN] = MPI_COMM_NULL;
+			directions[UP] = MPI_COMM_NULL;
+			directions[DOWN] = rank_comm[13];
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
 		} break;
@@ -156,7 +164,7 @@ int main(int count, char *argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
 	MPI_Comm_group(MPI_COMM_WORLD, &world_group);
-	for (int i = 0; i < 13; i++) {
+	for (int i = 0; i < 16; i++) {
 		MPI_Group_incl(world_group, 2, ranks[i], &groups[i]);
 		MPI_Comm_create(MPI_COMM_WORLD, groups[i], &rank_comm[i]);
 	}
@@ -167,7 +175,7 @@ int main(int count, char *argv[]) {
 	// MPI_COMM_NULL. Using MPI_COMM_NULL for MPI_Comm_rank or
 	// MPI_Comm_size is erroneous
 
-	for (int i = 0; i < 13; i++) {
+	for (int i = 0; i < 16; i++) {
 		if (MPI_COMM_NULL != rank_comm[i]) {
 			MPI_Comm_rank(rank_comm[i], &void_rank);
 			MPI_Comm_size(rank_comm[i], &void_size);
