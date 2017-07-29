17f0fb05b65f1546520844fd8ee6d51a28b7a933
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2017-07-28 22:12:11 -0300

    dispatcher + processor

--- Aulas/src/mpi2mmgrid.c
+++ Aulas/src/mpi2mmgrid.c
@@ -14,10 +14,10 @@
 MPI_Group world_group;
 
 // Construct a group containing all of the ranks in world_group
-MPI_Group groups[16] = { MPI_GROUP_NULL };
+MPI_Group groups[16];
 
 // Create a new communicator based on the group
-MPI_Comm rank_comm[16] = { MPI_COMM_NULL }, directions[4] = { MPI_COMM_NULL };
+MPI_Comm rank_comm[16], directions[4];
 
 // Requests
 MPI_Request request[4];
@@ -25,31 +25,34 @@ MPI_Request request[4];
 // Status
 MPI_Status STATUS;
 
+// Transfer
+double *message = (double *) malloc(sizeof(double) * 512 * 512 * 4 + 4);
+
 // Ranks relation
-const int ranks[16][2] = {  { 0,  1},
-							{ 0,  3},
-							{ 0,  9},
-							{ 1,  2},
-							{ 1,  4},
-							{ 2,  5},
-							{ 2, 10},
-							{ 3,  4},
-							{ 3,  6},
-							{ 4,  5},
-							{ 4,  7},
-							{ 5,  8},
-							{ 6,  7},
-							{ 6, 12},
-							{ 7,  8},
-							{ 8, 11}
-						};
+const int ranks[16][2] = {{0, 1},
+                          {0, 3},
+                          {0, 9},
+                          {1, 2},
+                          {1, 4},
+                          {2, 5},
+                          {2, 10},
+                          {3, 4},
+                          {3, 6},
+                          {4, 5},
+                          {4, 7},
+                          {5, 8},
+                          {6, 7},
+                          {6, 12},
+                          {7, 8},
+                          {8, 11}
+};
 
 
 // Sizes
 int world_rank, world_size;
 
 void init() {
-	switch(world_rank) {
+	switch (world_rank) {
 		/*
 		R    U   D   L   R
 		00 { 01, 02, -1, 00 }
@@ -78,126 +81,138 @@ void init() {
 			directions[DOWN] = rank_comm[2];
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = rank_comm[0];
-		} break;
+		}
+			break;
 
 		case 1: {
 			directions[UP] = rank_comm[4];
 			directions[DOWN] = MPI_COMM_NULL;
 			directions[LEFT] = rank_comm[0];
 			directions[RIGHT] = rank_comm[3];
-		} break;
+		}
+			break;
 
 		case 2: {
 			directions[UP] = rank_comm[5];
 			directions[DOWN] = rank_comm[6];
 			directions[LEFT] = rank_comm[3];
 			directions[RIGHT] = MPI_COMM_NULL;
-		} break;
+		}
+			break;
 
 		case 3: {
 			directions[UP] = rank_comm[8];
 			directions[DOWN] = rank_comm[1];
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = rank_comm[7];
-		} break;
+		}
+			break;
 
 		case 4: {
 			directions[UP] = rank_comm[10];
 			directions[DOWN] = rank_comm[4];
 			directions[LEFT] = rank_comm[7];
 			directions[RIGHT] = rank_comm[9];
-		} break;
+		}
+			break;
 
 		case 5: {
 			directions[UP] = rank_comm[11];
 			directions[DOWN] = rank_comm[5];
 			directions[LEFT] = rank_comm[9];
 			directions[RIGHT] = MPI_COMM_NULL;
-		} break;
+		}
+			break;
 
 		case 6: {
 			directions[UP] = rank_comm[13];
 			directions[DOWN] = rank_comm[8];
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = rank_comm[12];
-		} break;
+		}
+			break;
 
 		case 7: {
 			directions[UP] = MPI_COMM_NULL;
 			directions[DOWN] = rank_comm[10];
 			directions[LEFT] = rank_comm[12];
 			directions[RIGHT] = rank_comm[14];
-		} break;
+		}
+			break;
 
 		case 8: {
 			directions[UP] = rank_comm[15];
 			directions[DOWN] = rank_comm[11];
 			directions[LEFT] = rank_comm[14];
 			directions[RIGHT] = MPI_COMM_NULL;
-		} break;
+		}
+			break;
 
 		case 9: {
 			directions[UP] = rank_comm[2];
 			directions[DOWN] = MPI_COMM_NULL;
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
-		} break;
+		}
+			break;
 
 		case 10: {
 			directions[UP] = rank_comm[6];
 			directions[DOWN] = MPI_COMM_NULL;
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
-		} break;
+		}
+			break;
 
 		case 11: {
 			directions[UP] = MPI_COMM_NULL;
 			directions[DOWN] = rank_comm[15];
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
-		} break;
+		}
+			break;
 
 		case 12: {
 			directions[UP] = MPI_COMM_NULL;
 			directions[DOWN] = rank_comm[13];
 			directions[LEFT] = MPI_COMM_NULL;
 			directions[RIGHT] = MPI_COMM_NULL;
-		} break;
+		}
+			break;
 	}
 }
 
 void grid() {
-	int group_rank[4] = { -1 }, completed_total = 0, test_result[4] = { 0 }, completed[4] = { 0 }, busy = 0;
-	double data[2] = { 0 };
+	int group_rank[4], completed_total = 0, test_result[4], completed[4], busy = 0;
+	double data[4][1048580];
 
-    for (int i = 0; i < 4; i++) {
-        request[i] = MPI_REQUEST_NULL;
-    }
+	for (int i = 0; i < 4; i++) {
+		request[i] = MPI_REQUEST_NULL;
+		group_rank[i] = -1;
+		test_result[i] = 0;
+		completed[i] = 0;
+		if (directions[i] != MPI_COMM_NULL)
+			MPI_Comm_rank(directions[i], &group_rank[i]);
+        printf("%d/%d/%s: Watching in the mirrors.\n", world_rank, group_rank[i],
+               i == UP ? "UP" : i == DOWN ? "DOWN" : i == LEFT ? "LEFT" : "RIGHT");
+	}
 
-	while(1) {
-		sleep(1);
+	while (1) {
 		// Listen to the sides
-		for (int i = 0; i < 4; i++) {
-			if (directions[i] != MPI_COMM_NULL) {
-				MPI_Comm_rank(directions[i], &group_rank[i]);
-                if (request[i] == MPI_REQUEST_NULL) {
-					if (world_rank == 0)
-						MPI_Irecv(&data[0], 1, MPI_DOUBLE, 1, TAG, directions[i], &request[i]);
-					else
-						MPI_Irecv(&data[0], 1, MPI_DOUBLE, 0, TAG, directions[i], &request[i]);
-				}
-			}
-		}
+		for (int i = 0; i < 4; i++)
+			if (directions[i] != MPI_COMM_NULL)
+				if (request[i] == MPI_REQUEST_NULL)
+					MPI_Irecv(&data[i], 1048580, MPI_DOUBLE, !group_rank[i], TAG, directions[i], &request[i]);
+
+
 		// Check for completed requests in the sides
-		while (!completed_total) {
+		while (completed_total == 0) {
 			for (int i = 0; i < 4; i++) {
 				if (directions[i] != MPI_COMM_NULL) {
-					if (request[i] != MPI_REQUEST_NULL) {
-						MPI_Test(&request[i], &test_result[completed_total], &STATUS);
-						if (test_result[i]) {
-							completed[completed_total] = i;
-							completed_total++;
-						}
+					MPI_Test(&request[i], &test_result[completed_total], &STATUS);
+					if (test_result[completed_total]) {
+						completed[completed_total] = i;
+						completed_total++;
 					}
 				}
 			}
@@ -210,80 +225,97 @@ void grid() {
 			09    10
 		*/
 		for (int i = 0; i < completed_total; i++) {
-			printf("Request from %d completed in %d.\n", completed[i], world_rank);
-			switch (completed[i]) {
-				case UP: {
+			sleep(2);
+			printf("[%d/%d] Request from %s completed in %d.\n", i, completed_total,
+			       completed[i] == UP ? "UP" : completed[i] == DOWN ? "DOWN" : completed[i] == LEFT ? "LEFT" : "RIGHT",
+			       world_rank);
+			switch (world_rank) {
+				case 2: {
 					// 09 / 00 / 03 / 06 / 01 / 04 / 10 / 05 / 08
-					switch (world_rank) {
-						// Processors
-						case 6:
-						case 8:
-							busy = 0;
+					if (completed[i] == DOWN)
+						busy = 0;
 
-						default: {
-							if (!busy) {
-								int pos;
-								srand(time(0));
-								while (completed[i] == (pos = rand() % 4));
-								if (group_rank[i] != -1) MPI_Send(data, 2, MPI_DOUBLE, !group_rank[i], TAG, directions[pos]);
-								busy = 1;
-							}
-						} break;
-					}
-				} break;
-
-				case DOWN: {
-					switch (world_rank) {
-						case 12:
-						case 9:
-							busy = 0;
-
-						default: {
-							if (!busy) {
-								int pos;
-								srand(time(0));
-								while (completed[i] == (pos = rand() % 4));
-								MPI_Send(data, 2, MPI_DOUBLE, !group_rank[i], TAG, directions[pos]);
-								busy = 1;
-							}
-						} break;
-					}
-				} break;
-
-				default: {
 					if (!busy) {
+						printf("%d: Ready to dispatch to processor %s.\n", world_rank, "10");
+						busy = 1;
+						MPI_Send(data, 2, MPI_DOUBLE, !group_rank[DOWN], TAG, directions[DOWN]);
+					} else {
 						int pos;
 						srand(time(0));
-						while (completed[i] == (pos = rand() % 4));
-						MPI_Send(data, 2, MPI_DOUBLE, !group_rank[i], TAG, directions[pos]);
-						busy = 1;
+						while (completed[i] == (pos = rand() % 4) || group_rank[pos] == -1);
+						MPI_Send(data, 2, MPI_DOUBLE, !group_rank[pos], TAG, directions[pos]);
 					}
-				} break;
+					break;
+				}
+
+				case 6:
+				case 8: {
+					if (completed[i] == UP)
+						busy = 0;
+
+					if (!busy) {
+						printf("%d: Ready to dispatch to processor %s.\n", world_rank, world_rank == 6 ? "12" : "11");
+						busy = 1;
+						MPI_Send(data, 2, MPI_DOUBLE, !group_rank[UP], TAG, directions[UP]);
+					} else {
+						int pos;
+						srand(time(0));
+						while (completed[i] == (pos = rand() % 4) || group_rank[pos] == -1);
+						MPI_Send(data, 2, MPI_DOUBLE, !group_rank[pos], TAG, directions[pos]);
+					}
+					break;
+				}
+
+				default: {
+					int pos;
+					srand(time(0));
+					while (completed[i] == (pos = rand() % 4) || group_rank[pos] == -1);
+					MPI_Send(data, 2, MPI_DOUBLE, !group_rank[pos], TAG, directions[pos]);
+					break;
+				}
 			}
 		}
 	}
 }
 
 void dispatcher(int req) {
-	double data[2] = { 0 };
+	int completed[req], completed_total = 0;
+	double data[2] = {0};
+	MPI_Request callback[req];
+
+	sleep(5);
+	printf("%d: Preparing to launch. Dispatching information.\n", world_rank);
 	for (int i = 0; i < req; i++) {
-		MPI_Send(data, 2, MPI_DOUBLE, 0, 1, directions[UP]);
+		sleep(1);
+		completed[i] = 0;
+		callback[req] = MPI_REQUEST_NULL;
+		MPI_Send(data, 2, MPI_DOUBLE, 0, TAG, directions[UP]);
+		MPI_Irecv(data, 2, MPI_DOUBLE, 0, TAG, directions[UP], &callback[i]);
 	}
-	MPI_Recv(data, 2, MPI_DOUBLE, 0, 0, directions[UP], &STATUS);
+	while (completed_total < req) {
+		for (int i = 0; i < 4; i++) {
+			if (directions[i] != MPI_COMM_NULL) {
+				MPI_Test(&callback[i], &completed[completed_total], &STATUS);
+				if (completed[completed_total])
+					completed_total++;
+			}
+		}
+	}
+//    MPI_Send(data, 2, MPI_DOUBLE, 0, TAG, directions[UP], &STATUS);
 }
 
 void process() {
-	double size[2] = { 0 };
-	int free = 1;
+	double data[2] = {0};
 	if (world_rank > 10) {
-		MPI_Recv(size, 2, MPI_DOUBLE, 0, 0, directions[DOWN], &STATUS);
+		MPI_Recv(data, 2, MPI_DOUBLE, 0, TAG, directions[DOWN], &STATUS);
 		// kernel_2mm();
-		MPI_Send(&free, 1, MPI_INT, 0, TAG, directions[DOWN]);
+		MPI_Send(data, 2, MPI_DOUBLE, 0, TAG, directions[DOWN]);
 	} else {
-		MPI_Recv(size, 2, MPI_DOUBLE, 0, 0, directions[UP], &STATUS);
+		MPI_Recv(data, 2, MPI_DOUBLE, 0, TAG, directions[UP], &STATUS);
 		// kernel_2mm();
-		MPI_Send(&free, 1, MPI_INT, 0, TAG, directions[UP]);
+		MPI_Send(data, 2, MPI_DOUBLE, 0, TAG, directions[UP]);
 	}
+	printf("%d: Ready to work.\n", world_rank);
 }
 
 int main(int count, char *argv[]) {
@@ -298,9 +330,10 @@ int main(int count, char *argv[]) {
 	}
 
 	init();
+	printf("%d: We're settled up to go. Preparing ultrasound interfaces.\n", world_rank);
 
 	if (world_rank == 9)
-		dispatcher(5);
+		dispatcher(1);
 	else if (world_rank < 9)
 		grid();
 	else
