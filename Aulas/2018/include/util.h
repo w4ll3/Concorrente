c90e52e256fe211aa3dd3b97de99fea5ac99700b
Gregório Granado Magalhães <greg.magalhaes@gmail.com>
2018-05-23 15:04:53 -0300

    nthreads?

--- Aulas/2018/include/util.h
+++ Aulas/2018/include/util.h
@@ -1,17 +1,25 @@
+#include <omp.h>
+#include <math.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <limits.h>
+#include <string.h>
 #include <getopt.h>
 #include <pthread.h>
-#include <time.h>
+#include <sys/time.h>
 #include <semaphore.h>
-#include <omp.h>
-#include <limits.h>
 
 #ifdef MPI
 	#include "mpi.h"
 #endif
+
+#define MICROSEC 0.000001
+
+extern int nthreads;
+extern int verbose;
 
 void freetrix(float **m_a, int size);
 
@@ -24,3 +32,100 @@ double elapsed;
 					elapsed = (finish.tv_sec - start.tv_sec); \
 					elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0; \
 					printf("%lf\n", elapsed);
+
+
+// Prints an error message and exits.
+extern void error(const char *msg);
+
+// Generates a normal number.
+extern double normalnum(double mu, double sigma);
+
+// Generates a random number.
+extern unsigned randnum(void);
+
+// Generates a pseudo-random real number uniformly distributed between 0.0 and 1.0. 
+extern double prngnum(void);
+extern void prng_set_seed(int seed);
+
+// Safe calloc().
+extern void *scalloc(size_t nmemb, size_t size);
+
+// Safe malloc().
+extern void *smalloc(size_t size);
+
+// Initializes the random number generator.
+extern void srandnum(int seed);
+
+// Safe realloc().
+extern void *srealloc(void *p, size_t size);
+
+// Prints a warning message.
+extern void warning(const char *msg);
+
+// Initializes power measurement utility.
+extern void power_init(void);
+
+// Terminates power measurement utility.
+extern double power_end(void);
+
+// Computers the difference between two timers.
+extern uint64_t timer_diff(uint64_t t1, uint64_t t2);
+
+// Gets the current timer value
+extern uint64_t timer_get(void);	
+
+// Initializes the timer.
+extern void timer_init(void);
+
+//Vector.
+struct vector {
+	int size;        /* Size.     */
+	float *elements; /* Elements. */
+};
+
+//Opaque pointer to a vector.
+typedef struct vector * vector_t;
+
+//Opaque pointer to a constant vector.
+typedef const struct vector * const_vector_t;
+
+//Clusters data. 
+int *kmeans(vector_t *data, int npoints, int ncentroids, float mindistance);
+
+//Returns the size of a vector.
+#define vector_size(v) \
+	(((vector_t) (v))->size)
+
+//Clears a vector.
+extern void vector_clear(struct vector *v);
+
+//Creates a vector.
+extern vector_t vector_create(int n);
+
+//Destroys a vector.
+extern void vector_destroy(vector_t v);
+
+//Computes the euclidean distance between two points.
+extern float vector_distance(const_vector_t a, const_vector_t b);
+
+//Tests if two vectors are equal.
+extern int vector_equal(const_vector_t a, const_vector_t b);
+
+//Assigns a vector to another.
+extern vector_t vector_assign(vector_t v1, const_vector_t v2);
+
+//Subtracts two vectors.
+extern vector_t vector_sub(vector_t v1, const_vector_t v2);
+
+//Adds two vectors.
+extern vector_t vector_add(vector_t v1, const_vector_t v2);
+
+//Multiplies a vector by a scalar.
+extern vector_t vector_mult(vector_t v, float scalar);
+
+//Fills up vector with random numbers.
+extern vector_t vector_random(vector_t v);
+
+//Returns the element [i] in a vector.
+#define VECTOR(v, i) \
+	(((vector_t)(v))->elements[(i)])
--- Aulas/2018/src/util.c
+++ Aulas/2018/src/util.c
@@ -7,3 +7,309 @@ void freetrix(float **m_a, int size) {
 	m_a = 0;
 	free(m_a);
 }
+
+/*
+ * Prints an error message and exits.
+ */
+void error(const char *msg) {
+	fprintf(stderr, "error: %s\n", msg);
+	exit(EXIT_FAILURE);
+}
+
+/*
+ * Prints a warning message.
+ */
+void warning(const char *msg) {
+	fprintf(stderr, "warning: %s\n", msg);
+}
+
+/*
+ * Safe malloc().
+ */
+void *smalloc(size_t size) {
+	void *p;
+	
+	p = malloc(size);
+	
+	/* Failed to allocate memory. */
+	if (p == NULL)
+		error("cannot malloc()");
+	
+	return (p);
+}
+
+/*
+ * Safe calloc().
+ */
+void *scalloc(size_t nmemb, size_t size) {
+	void *p;
+	
+	p = calloc(nmemb, size);
+	
+	/* Failed to allocate memory. */
+	if (p == NULL)
+		error("cannot calloc()");
+	
+	return (p);
+}
+
+/*
+ * Safe realloc().
+ */
+void *srealloc(void *p, size_t size) {
+	
+	p = realloc(p, size);
+	
+	/* Failed to allocate memory. */
+	if (p == NULL)
+		error("cannot realloc()");
+	
+	return (p);
+}
+
+
+#define RANDNUM_W 521288629;
+#define RANDNUM_Z 362436069;
+
+static unsigned randum_w = RANDNUM_W;
+static unsigned randum_z = RANDNUM_Z;
+
+/*
+ * Initializes the random number generator.
+ */
+void srandnum(int seed) {
+	unsigned w, z;
+
+	w = (seed * 104623) & 0xffffffff;
+	randum_w = (w) ? w : RANDNUM_W;
+	z = (seed * 48947) & 0xffffffff;
+	randum_z = (z) ? z : RANDNUM_Z;
+}
+
+/*
+ * Generates a random number.
+ */
+unsigned randnum(void) {
+	unsigned u;
+	
+	/* 0 <= u < 2^32 */
+	randum_z = 36969 * (randum_z & 65535) + (randum_z >> 16);
+	randum_w = 18000 * (randum_w & 65535) + (randum_w >> 16);
+	u = (randum_z << 16) + randum_w;
+	
+	return (u);
+}
+
+/*
+ * Generates a normal number.
+ */
+double normalnum(double mu, double sigma) {
+	double U1, U2, W, mult;
+	static double X1, X2;
+	static int call = 0;
+ 
+	if (call == 1) {
+		call = !call;
+		return (mu + sigma * (double) X2);
+	}
+ 
+	do {
+		U1 = -1 + ((double) randnum() / RAND_MAX) * 2;
+		U2 = -1 + ((double) randnum() / RAND_MAX) * 2;
+		W = pow (U1, 2) + pow (U2, 2);
+	} while (W >= 1 || W == 0);
+ 
+	mult = sqrt ((-2 * log (W)) / W);
+	X1 = U1 * mult;
+	X2 = U2 * mult;
+ 
+	call = !call;
+ 
+	return (mu + sigma * (double) X1);
+}
+
+/*
+ * pRNG based on http://www.cs.wm.edu/~va/software/park/park.html
+ *
+ * Returns a pseudo-random real number uniformly distributed 
+ * between 0.0 and 1.0. 
+ */
+#define MODULUS    2147483647
+#define MULTIPLIER 48271
+
+static long prngseed = 0;
+double prngnum() {
+  const long Q = MODULUS / MULTIPLIER;
+  const long R = MODULUS % MULTIPLIER;
+        long t;
+
+  t = MULTIPLIER * (prngseed % Q) - R * (prngseed / Q);
+  if (t > 0) 
+    prngseed = t;
+  else 
+    prngseed = t + MODULUS;
+  return ((double) prngseed / MODULUS);
+}
+
+void prng_set_seed(int seed) {
+	prngseed = seed;
+}
+
+/* Timer residual error. */
+static uint64_t timer_error = 0;
+
+/*
+ * Gets the current timer value.
+ */
+uint64_t timer_get(void) {
+	uint64_t ret;
+	struct timeval t;
+
+	gettimeofday(&t, NULL);
+	ret = 1000000 * ((uint64_t) t.tv_sec);
+	ret += (uint64_t) t.tv_usec;
+
+	return ret;
+}
+
+/*
+ * Computers the difference between two timers.
+ */
+uint64_t timer_diff(uint64_t t1, uint64_t t2) {
+	return (t2 - t1 - timer_error);
+}
+
+/*
+ * Initializes the timer.
+ */
+void timer_init(void) {
+  uint64_t start, end;
+  
+  start = timer_get();
+  end = timer_get();
+  
+  timer_error = (end - start);
+}
+
+// Creates a vector.
+struct vector *vector_create(int n) {
+	struct vector *v;
+	
+	v = smalloc(sizeof(struct vector));
+	
+	/* Initilize vector. */
+	v->size = n;
+	v->elements = calloc(n, sizeof(float));
+
+	return (v);
+}
+
+// Adds two vectors.
+struct vector *vector_add(struct vector *v1, const struct vector *v2) {
+	int i; /* Loop index.  */
+	int n; /* Vector size. */
+	
+	n = vector_size(v1);
+	
+	/* Add vectors. */
+	for (i = 0; i < n; i++)
+		VECTOR(v1, i) += VECTOR(v2, i);
+	
+	return (v1);
+}
+
+// Assigns a vector to another.
+struct vector *vector_assign(struct vector *v1, const struct vector *v2) {
+	int i; /* Loop index.  */
+	int n; /* Vector size. */
+	
+	n = vector_size(v1);
+	
+	/* Add vectors. */
+	for (i = 0; i < n; i++)
+		VECTOR(v1, i) = VECTOR(v2, i);
+	
+	return (v1);
+}
+
+// Destroys a vector.
+void vector_destroy(struct vector *v) {
+	free(v->elements);
+	free(v);
+}
+
+// Computes the euclidean distance between two points.
+float vector_distance(const struct vector *a, const struct vector *b) {
+	int i;          /* Loop index. */
+	float distance; /* Distance.   */
+
+	distance = 0;
+
+	/* Computes the euclidean distance. */
+	for (i = 0; i < a->size; i++)
+		distance +=  pow(VECTOR(a, i) - VECTOR(b, i), 2);
+	distance = sqrt(distance);
+	
+	return (distance);
+}
+
+// Tests if two vectors are equal.
+int vector_equal(const struct vector *a, const struct vector *b) {
+	int i;
+	
+	/* Test all elements. */
+	for (i = 0; i < a->size; i++) {
+		if (VECTOR(a, i) != VECTOR(b, i))
+			return (0);
+	}
+	
+	return (1);
+}
+
+// Multiplies a vector by a scalar.
+struct vector *vector_mult(struct vector *v, float scalar) {
+	int i; /* Loop index.  */
+	int n; /* Vector size. */
+	
+	n = vector_size(v);
+	
+	/* Add vectors. */
+	for (i = 0; i < n; i++)
+		VECTOR(v, i) *= scalar;
+	
+	return (v);
+}
+
+// Fills up vector with random numbers.
+struct vector *vector_random(struct vector *v) {
+	int i;
+	
+	/* Fill vector. */
+	for (i = 0; i < vector_size(v); i++)
+		VECTOR(v, i) = randnum() & 0xffff;
+
+	return (v);
+}
+
+// Subtracts two vectors.
+struct vector *vector_sub(struct vector *v1,const struct vector *v2) {
+	int i; /* Loop index.  */
+	int n; /* Vector size. */
+	
+	/* Invalid argument. */
+	if (vector_size(v1) != vector_size(v2))
+		return (NULL);
+	
+	n = vector_size(v1);
+	
+	/* Subtract vectors. */
+	for (i = 0; i < n; i++)
+		VECTOR(v1, i) -= VECTOR(v2, i);
+	
+	return (v1);
+}
+
+// Clears a vector.
+void vector_clear(struct vector *v) {
+	memset(v->elements, 0, sizeof(float)*vector_size(v));	
+}
